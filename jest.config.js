cf85b9e9624aab8e19e46f936e9ffc751e9c7b0d
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2023-11-18 17:57:15 +0900

    Improve import

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  moduleNameMapper: {
+    "^@/(.+)$": "<rootDir>/src/$1",
+  },
   preset: "ts-jest",
   roots: ["<rootDir>/src"],
   testEnvironment: "jest-environment-jsdom",
